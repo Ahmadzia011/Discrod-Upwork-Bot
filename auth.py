211530a3631b62084aa568c6e2bdb76db1a19295
Ahmad zia <ahmadzia0304@gmail.com>
2026-03-23 07:01:33 +0500

    Retrying

--- auth.py
+++ auth.py
@@ -20,6 +20,3 @@ def get_cookies(jobQuery):
     with open("cookies.json", "w") as file:
         json.dump(loaded_cookies, file, indent=4)
 
-
-MTQ3NjA5NzQ0MTc4MTY0OTUxMA.Gb3vXx.Ef8E8FCDiHBZNZhjIzZeNCDc6r_ZgEBMzd8KJ0
-MTQ3NjA5NzQ0MTc4MTY0OTUxMA.GsaUqT.oLGS-k-VuJUYy3Q9n5lwH4D2_UJkyr0A637eLk
